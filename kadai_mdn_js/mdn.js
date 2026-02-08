bb43930aa4ee535a9a8b4a4e4cdbc1599c0e88b7
roypoon <roypoon1993@gmail.com>
2026-02-08 18:25:07 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,9 @@
-const today = new Date("2026-02-03");
+const today = new Date();
+
+const month = today.getMonth()+1 
 
 const getDate = () => {
-  console.log(today.getFullYear() + "年" + today.getMonth() + "月" + today.getDate() + "日");
+  console.log(today.getFullYear() + "年" + month + "月" + today.getDate() + "日");
 }
 
 getDate()
